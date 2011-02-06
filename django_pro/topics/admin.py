cb44b50150e41ccc6e02415fc13c327dc0eb5337
mushfiq <mushfiq.it@gmail.com>
2011-02-06 22:37:00 +0600

    activated topic backend  CRUD

---
+++ django_pro/topics/admin.py
@@ -0,0 +1,5 @@
+from topics.models import Topics
+from django.contrib import admin
+
+
+admin.site.register(Topics)
--- django_pro/topics/models.py
+++ django_pro/topics/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+class Admin:
+	list_display = ('author','date')
 class Topics(models.Model):
 	topic_name = models.CharField(max_length=200)
+	image_url = models.CharField(max_length=100)
+	topic_guid = models.CharField(max_length=100)
+ 
+class Posts(models.Model):
+	author = models.ForeignKey(User)
+	date = models.DateTimeField()
+	title = models.CharField(max_length=100)
+	post = models.TextField()
+	
 	
 
